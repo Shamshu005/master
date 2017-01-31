364c4c23fc8afedc44d0d8fc386a9862fccd9269
shamshuddin <shamshu005@gmail.com>
2017-01-31 14:28:25 +0530

    sssssss

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var db = mongojs('mongodb://localhost:27017/mydb')
 
 //var db = mongojs('http://localhost:27017/mydb', ['collection']);
 app.get('/', function(req, res) {
-  res.send("h");
+  res.send("hxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
     // var obj = {
     //     "aa": "aa"
     // };
@@ -27,5 +27,5 @@ app.get('/', function(req, res) {
 //     console.log('Example app listening on port 3000!');
 // })
 app.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', app.address().port);
+  console.log('listening on');
 });
